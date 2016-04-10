340e6174c5b8b3ca48db30f09fb4e79fccab65ee
Rolf Erik Lekang <me@rolflekang.com>
2016-04-10 19:05:49 +0200

    chore: Revive project
    Upgrade to django 1.9 and use django 1.9 project template. It did not
    run on python 3.5 so had to upgrade.

---
+++ photos/settings.py
@@ -0,0 +1,83 @@
+import json
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+REPO_DIR = os.path.dirname(BASE_DIR)
+
+
+def load_secrets():
+    try:
+        with open(os.path.join(REPO_DIR, 'secrets.json')) as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+secrets = load_secrets()
+
+DEBUG = secrets.get('debug', True)
+ALLOWED_HOSTS = secrets.get('allowed_hosts', [])
+SECRET_KEY = secrets.get('secret_key', 'a secret')
+# AUTH_USER_MODEL = 'users.User'
+
+
+INSTALLED_APPS = [
+    'photos.gallery',
+
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'photos.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'photos.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': secrets.get('db_name', 'photos'),
+    }
+}
+
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(REPO_DIR, 'static')
+
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(REPO_DIR, 'uploads')

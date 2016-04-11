6c25c603e410a5b00e2a9ed074a33561e977e735
Rolf Erik Lekang <me@rolflekang.com>
2016-04-11 06:53:55 +0200

    feat: Filter photos by domain

--- photos/gallery/views.py
+++ photos/gallery/views.py
@@ -16,6 +16,10 @@ class PhotoDetailView(DetailView):
     def get_object(self, queryset=None, **kwargs):
         queryset = queryset or self.get_queryset()
         slug = self.kwargs.get(self.slug_url_kwarg, None)
+        host = self.request.get_host()
+
+        if host != 'photos.mocco.no':
+            queryset = queryset.filter(user__domain=host)
 
         if slug is None:
             return queryset.last()
@@ -24,3 +28,12 @@ class PhotoDetailView(DetailView):
 
 class ArchiveView(ListView):
     model = Photo
+
+    def get_queryset(self):
+        queryset = self.model.objects.all()
+        host = self.request.get_host()
+
+        if host != 'photos.mocco.no':
+            queryset = queryset.filter(user__domain=host)
+
+        return queryset
--- photos/users/models.py
+++ photos/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    domain = models.CharField(max_length=200, blank=True, db_index=True)

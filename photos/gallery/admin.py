b2d5ca48c6495c8d4d05b6d76df1c4b0a146ca2b
Rolf Erik Lekang <me@rolflekang.com>
2016-04-12 21:38:22 +0200

    fix: Make django admin default to current user
    Make user field not editable for non superuser

--- photos/gallery/admin.py
+++ photos/gallery/admin.py
@@ -8,3 +8,12 @@ class PhotoAdmin(admin.ModelAdmin):
     list_display = ('admin_thumbnail', 'title', 'file', 'taken_at', 'camera', 'lens',
                     'focal_length', 'exposure_time', 'aperture', 'iso')
     date_hierarchy = 'taken_at'
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        if db_field.name == 'user':
+            kwargs['initial'] = request.user.id
+
+        if not request.user.is_superuser:
+            kwargs['disabled'] = True
+
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
